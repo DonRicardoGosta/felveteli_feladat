52c5b90ea066fa7699015d6585f86b77ab528c80
donricardogosta <horvathdanii99@gmail.com>
2021-09-30 13:31:39 +0200

    Feature: Data endpoint

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
     MSG = None
     if request.method == 'POST':
@@ -16,6 +16,8 @@ def login():
                 session['username'] = request.form['username']
                 session['password'] = request.form['password']
                 MSG = "7a55204fb9d7076b6d73b3bc5d8ed2849d86a26e"
+                request.headers["x-api-key"] = MSG
+                MSG = request.headers.get('x.api.key')
             else:
                 MSG = "401 Unauthorized"
         else:
@@ -24,10 +26,22 @@ def login():
     return render_template("login.html")
 
 
+@app.route('/data')
+def data():
+    if request.headers.get('x.api.key') != "401 Unauthorized" and request.headers.get('x.api.key') is not None:
+        response_data = {
+                        "id": 1,
+                        "text": "Kacsa",
+                        "year": 1964}
+
+        return response_data
+    else:
+        return "401 Unauthorized"
+
+
 def main():
     print("main")
 
 
 if __name__ == '__main__':
     main()
-
